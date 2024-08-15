c8548bc757986abde27b6fc443a17936a73328d1
Chosen_1st <Chosen_1st@Outlook.com>
2024-08-15 10:26:24 +0800

    Update Docs

---
+++ README.md
@@ -0,0 +1,18 @@
+# RSerene-Wiki
+## 安装
+
+安装该项目所需的依赖
+
+```bash
+npm install
+```
+
+
+
+## 运行
+
+编译并运行该项目
+
+```bash
+npm run docs:dev
+```
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,57 @@
+import { defineUserConfig } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  title: "vuepress-theme-reco",
+  description: "Just playing around",
+  bundler: viteBundler(),
+  // bundler: webpackBundler(),
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "reco_luan",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "Home", link: "/" },
+      {
+        text: "Docs",
+        children: [
+          { text: "vuepress-reco", link: "/theme-reco/theme" },
+          { text: "vuepress-theme-reco", link: "/other/guide" },
+        ],
+      },
+    ],
+    // commentConfig: {
+    //   type: 'valine',
+    //   // options 与 1.x 的 valineConfig 配置一致
+    //   options: {
+    //     // appId: 'xxx',
+    //     // appKey: 'xxx',
+    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
+    //     // verify: true, // 验证码服务
+    //     // notify: true,
+    //     // recordIP: true,
+    //     // hideComments: true // 隐藏评论
+    //   },
+    // },
+  }),
+  // debug: true,
+});
