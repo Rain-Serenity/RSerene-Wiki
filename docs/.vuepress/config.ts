4cbece4f0b419ac969f3ac22efbb03ff487850aa
Chosen_1st <Chosen_1st@Outlook.com>
2024-08-16 09:21:54 +0800

    Update Docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -34,10 +34,15 @@ export default defineUserConfig({
             text: '玩家手册',
             children: ['/en/guide/introduction.md', '/en/guide/settings.md', '/en/guide/command.md', '/en/guide/faq.md', '/en/guide/sponsor.md'],
           }],
+          '/en/changelog/': [{
+            text: '更新日志',
+            children: ['/en/changelog/changelog.md'],
+          }],
         },
         navbar: [
           { text: "主页", link: "/en/" },
           { text: '玩家手册', children: [ {text: '简单介绍', link: '/en/guide/introduction.html'}, {text: '基本设定', link: '/en/guide/settings.html'}, {text: '指令列表', link: '/en/guide/command.html'}, {text: '常见问题', link: '/en/guide/faq.html'} ] },
+          { text: '更新日志', children: [ {text: '更新日志', link: '/en/guide/changelog.html'} ] },
           { text: '相关链接', children: [ {text: 'Copilot', link: 'https://Copilot.RSerene.com/'}, {text: 'GitHub', link: 'https://GitHub.com/Rain-Serenity/'}, {text: 'MineBBS', link: 'https://www.MineBBS.com/threads/1-21.28390/'}, {text: 'Minecraft Wiki', link: 'https://Minecraft.Wiki/'}, {text: 'Status', link: 'https://Stats.UptimeRobot.com/ErgTAP18m2'} ]}
         ],
         bulletin: {
@@ -77,10 +82,15 @@ export default defineUserConfig({
             text: '玩家手册',
             children: ['/guide/introduction.md', '/guide/settings.md', '/guide/command.md', '/guide/faq.md', '/guide/sponsor.md'],
           }],
+          '/changelog/': [{
+            text: '更新日志',
+            children: ['/changelog/changelog.md'],
+          }],
         },
         navbar: [
           { text: "主页", link: "/" },
           { text: '玩家手册', children: [ {text: '简单介绍', link: '/guide/introduction.html'}, {text: '基本设定', link: '/guide/settings.html'}, {text: '指令列表', link: '/guide/command.html'}, {text: '常见问题', link: '/guide/faq.html'} ] },
+          { text: '更新日志', children: [ {text: '更新日志', link: '/guide/changelog.html'} ] },
           { text: '相关链接', children: [ {text: 'Copilot', link: 'https://Copilot.RSerene.com/'}, {text: 'GitHub', link: 'https://GitHub.com/Rain-Serenity/'}, {text: 'MineBBS', link: 'https://www.MineBBS.com/threads/1-21.28390/'}, {text: 'Minecraft Wiki', link: 'https://Minecraft.Wiki/'}, {text: 'Status', link: 'https://Stats.UptimeRobot.com/ErgTAP18m2'} ]}
         ],
         bulletin: {
