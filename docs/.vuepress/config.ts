635c4aab8c96a177cbc890cff0f0030bb9626d26
Chosen_1st <Chosen_1st@Outlook.com>
2026-06-27 17:08:19 +0800

    Update docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -130,7 +130,7 @@ export default defineUserConfig({
           body: [
             {
               type: 'text',
-              content: `🎉🎉🎉 伺服器已經更新到26.1.2。`,
+              content: `🎉🎉🎉 伺服器已經更新到26.2。`,
               style: 'font-size: 12px;'
             },
             { type: 'hr' },
@@ -169,21 +169,21 @@ export default defineUserConfig({
           }],
           '/en-us/about/': [{
             text: 'About Server',
-            children: ['/en-us/about/introduction.md', '/en-us/about/rules.md', '/en-us/about/history.md', '/en-us/about/contributions.md', '/en-us/about/sponsor.md'],
+            children: ['/en-us/about/introduction.md', '/en-us/about/history.md', '/en-us/about/contributions.md', '/en-us/about/rules.md', '/en-us/about/sponsor.md'],
           }],
         },
         navbar: [
           { text: "Home", link: "/en-us/" },
           { text: 'Survival Mode', children: [ {text: 'Introduction', link: '/en-us/survival/introduction.html'}, {text: 'Settings', link: '/en-us/survival/settings.html'}, {text: 'Command List', link: '/en-us/survival/command.html'}, {text: 'FAQ', link: '/en-us/survival/faq.html'} ] },
           { text: 'Skyblock Mode', children: [ {text: 'Introduction', link: '/en-us/island/introduction.html'}, {text: 'Settings', link: '/en-us/island/settings.html'}, {text: 'Command List', link: '/en-us/island/command.html'}, {text: 'FAQ', link: '/en-us/island/faq.html'} ] },
-          { text: 'About Us', children: [ {text: 'Server Introduction', link: '/en-us/about/introduction.html'}, {text: 'Server Rules', link: '/en-us/about/rules.html'}, {text: 'Server History', link: '/en-us/about/history.html'}, {text: 'Contributors', link: '/en-us/about/contributions.html'}, {text: 'Sponsor', link: '/en-us/about/sponsor.html'} ] },
+          { text: 'About Us', children: [ {text: 'Server Introduction', link: '/en-us/about/introduction.html'}, {text: 'Server History', link: '/en-us/about/history.html'}, {text: 'Contributors', link: '/en-us/about/contributions.html'}, {text: 'Server Rules', link: '/en-us/about/rules.html'}, {text: 'Sponsor', link: '/en-us/about/sponsor.html'} ] },
           { text: 'Related Links', children: [ {text: 'Ban List', link: 'https://Bans.RSerene.com/'}, {text: 'GitHub', link: 'https://GitHub.com/Rain-Serenity/'}, {text: 'MCBBS Memorial', link: 'https://www.MCBBS.co/thread-5031-1-1.html'}, {text: 'MineBBS', link: 'https://www.MineBBS.com/threads/1-21-1.28390/'}, {text: 'Minecraft Wiki', link: 'https://Minecraft.Wiki/'} ]}
         ],
         bulletin: {
           body: [
             {
               type: 'text',
-              content: `🎉🎉🎉 The server has been updated to 26.1.2.`,
+              content: `🎉🎉🎉 The server has been updated to 26.2.`,
               style: 'font-size: 12px;'
             },
             { type: 'hr' },
@@ -222,21 +222,21 @@ export default defineUserConfig({
           }],
           '/ru-ru/about/': [{
             text: 'О сервере',
-            children: ['/ru-ru/about/introduction.md', '/ru-ru/about/rules.md', '/ru-ru/about/history.md', '/ru-ru/about/contributions.md', '/ru-ru/about/sponsor.md'],
+            children: ['/ru-ru/about/introduction.md', '/ru-ru/about/history.md', '/ru-ru/about/contributions.md', '/ru-ru/about/rules.md', '/ru-ru/about/sponsor.md'],
           }],
         },
         navbar: [
           { text: "Главная", link: "/ru-ru/" },
           { text: 'Выживание', children: [ {text: 'Введение', link: '/ru-ru/survival/introduction.html'}, {text: 'Настройки', link: '/ru-ru/survival/settings.html'}, {text: 'Команды', link: '/ru-ru/survival/command.html'}, {text: 'FAQ', link: '/ru-ru/survival/faq.html'} ] },
           { text: 'Островной режим', children: [ {text: 'Введение', link: '/ru-ru/island/introduction.html'}, {text: 'Настройки', link: '/ru-ru/island/settings.html'}, {text: 'Команды', link: '/ru-ru/island/command.html'}, {text: 'FAQ', link: '/ru-ru/island/faq.html'} ] },
-          { text: 'О нас', children: [ {text: 'О сервере', link: '/ru-ru/about/introduction.html'}, {text: 'Правила', link: '/ru-ru/about/rules.html'}, {text: 'История', link: '/ru-ru/about/history.html'}, {text: 'Авторы', link: '/ru-ru/about/contributions.html'}, {text: 'Спонсорство', link: '/ru-ru/about/sponsor.html'} ] },
+          { text: 'О нас', children: [ {text: 'О сервере', link: '/ru-ru/about/introduction.html'}, {text: 'История', link: '/ru-ru/about/history.html'}, {text: 'Авторы', link: '/ru-ru/about/contributions.html'}, {text: 'Правила', link: '/ru-ru/about/rules.html'}, {text: 'Спонсорство', link: '/ru-ru/about/sponsor.html'} ] },
           { text: 'Ссылки', children: [ {text: 'Список банов', link: 'https://Bans.RSerene.com/'}, {text: 'GitHub', link: 'https://GitHub.com/Rain-Serenity/'}, {text: 'MCBBS Memorial', link: 'https://www.MCBBS.co/thread-5031-1-1.html'}, {text: 'MineBBS', link: 'https://www.MineBBS.com/threads/1-21-1.28390/'}, {text: 'Minecraft Wiki', link: 'https://Minecraft.Wiki/'} ]}
         ],
         bulletin: {
           body: [
             {
               type: 'text',
-              content: `🎉🎉🎉 Сервер обновлён до версии 26.1.2.`,
+              content: `🎉🎉🎉 Сервер обновлён до версии 26.2.`,
               style: 'font-size: 12px;'
             },
             { type: 'hr' },
@@ -275,21 +275,21 @@ export default defineUserConfig({
           }],
           '/ja-jp/about/': [{
             text: 'サーバーについて',
-            children: ['/ja-jp/about/introduction.md', '/ja-jp/about/rules.md', '/ja-jp/about/history.md', '/ja-jp/about/contributions.md', '/ja-jp/about/sponsor.md'],
+            children: ['/ja-jp/about/introduction.md', '/ja-jp/about/history.md', '/ja-jp/about/contributions.md', '/ja-jp/about/rules.md', '/ja-jp/about/sponsor.md'],
           }],
         },
         navbar: [
           { text: "ホーム", link: "/ja-jp/" },
           { text: 'サバイバル', children: [ {text: '紹介', link: '/ja-jp/survival/introduction.html'}, {text: '設定', link: '/ja-jp/survival/settings.html'}, {text: 'コマンド一覧', link: '/ja-jp/survival/command.html'}, {text: 'FAQ', link: '/ja-jp/survival/faq.html'} ] },
           { text: 'スカイブロック', children: [ {text: '紹介', link: '/ja-jp/island/introduction.html'}, {text: '設定', link: '/ja-jp/island/settings.html'}, {text: 'コマンド一覧', link: '/ja-jp/island/command.html'}, {text: 'FAQ', link: '/ja-jp/island/faq.html'} ] },
-          { text: 'サーバーについて', children: [ {text: 'サーバー紹介', link: '/ja-jp/about/introduction.html'}, {text: 'ルール', link: '/ja-jp/about/rules.html'}, {text: '歴史', link: '/ja-jp/about/history.html'}, {text: '貢献者', link: '/ja-jp/about/contributions.html'}, {text: 'スポンサー', link: '/ja-jp/about/sponsor.html'} ] },
+          { text: 'サーバーについて', children: [ {text: 'サーバー紹介', link: '/ja-jp/about/introduction.html'}, {text: '歴史', link: '/ja-jp/about/history.html'}, {text: '貢献者', link: '/ja-jp/about/contributions.html'}, {text: 'ルール', link: '/ja-jp/about/rules.html'}, {text: 'スポンサー', link: '/ja-jp/about/sponsor.html'} ] },
           { text: '関連リンク', children: [ {text: 'BANリスト', link: 'https://Bans.RSerene.com/'}, {text: 'GitHub', link: 'https://GitHub.com/Rain-Serenity/'}, {text: 'MCBBS Memorial', link: 'https://www.MCBBS.co/thread-5031-1-1.html'}, {text: 'MineBBS', link: 'https://www.MineBBS.com/threads/1-21-1.28390/'}, {text: 'Minecraft Wiki', link: 'https://Minecraft.Wiki/'} ]}
         ],
         bulletin: {
           body: [
             {
               type: 'text',
-              content: `🎉🎉🎉 サーバーは26.1.2に更新されました。`,
+              content: `🎉🎉🎉 サーバーは26.2に更新されました。`,
               style: 'font-size: 12px;'
             },
             { type: 'hr' },
@@ -328,21 +328,21 @@ export default defineUserConfig({
           }],
           '/ko-kr/about/': [{
             text: '서버 소개',
-            children: ['/ko-kr/about/introduction.md', '/ko-kr/about/rules.md', '/ko-kr/about/history.md', '/ko-kr/about/contributions.md', '/ko-kr/about/sponsor.md'],
+            children: ['/ko-kr/about/introduction.md', '/ko-kr/about/history.md', '/ko-kr/about/contributions.md', '/ko-kr/about/rules.md', '/ko-kr/about/sponsor.md'],
           }],
         },
         navbar: [
           { text: "홈", link: "/ko-kr/" },
           { text: '서바이벌', children: [ {text: '소개', link: '/ko-kr/survival/introduction.html'}, {text: '설정', link: '/ko-kr/survival/settings.html'}, {text: '명령어 목록', link: '/ko-kr/survival/command.html'}, {text: 'FAQ', link: '/ko-kr/survival/faq.html'} ] },
           { text: '스카이블록', children: [ {text: '소개', link: '/ko-kr/island/introduction.html'}, {text: '설정', link: '/ko-kr/island/settings.html'}, {text: '명령어 목록', link: '/ko-kr/island/command.html'}, {text: 'FAQ', link: '/ko-kr/island/faq.html'} ] },
-          { text: '서버 정보', children: [ {text: '서버 소개', link: '/ko-kr/about/introduction.html'}, {text: '규칙', link: '/ko-kr/about/rules.html'}, {text: '역사', link: '/ko-kr/about/history.html'}, {text: '기여자', link: '/ko-kr/about/contributions.html'}, {text: '후원', link: '/ko-kr/about/sponsor.html'} ] },
+          { text: '서버 정보', children: [ {text: '서버 소개', link: '/ko-kr/about/introduction.html'}, {text: '역사', link: '/ko-kr/about/history.html'}, {text: '기여자', link: '/ko-kr/about/contributions.html'}, {text: '규칙', link: '/ko-kr/about/rules.html'}, {text: '후원', link: '/ko-kr/about/sponsor.html'} ] },
           { text: '관련 링크', children: [ {text: '밴 목록', link: 'https://Bans.RSerene.com/'}, {text: 'GitHub', link: 'https://GitHub.com/Rain-Serenity/'}, {text: 'MCBBS Memorial', link: 'https://www.MCBBS.co/thread-5031-1-1.html'}, {text: 'MineBBS', link: 'https://www.MineBBS.com/threads/1-21-1.28390/'}, {text: 'Minecraft Wiki', link: 'https://Minecraft.Wiki/'} ]}
         ],
         bulletin: {
           body: [
             {
               type: 'text',
-              content: `🎉🎉🎉 서버가 26.1.2로 업데이트되었습니다.`,
+              content: `🎉🎉🎉 서버가 26.2로 업데이트되었습니다.`,
               style: 'font-size: 12px;'
             },
             { type: 'hr' },
